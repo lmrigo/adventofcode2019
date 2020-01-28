1d05ba9419065581ac47b758bebd4e3f6e079262
Lucas Rigo <lucas.rigo@sap.com>
2020-01-28 11:27:20 -0300

    refactored day 7 part 1 to prepare for part 2

--- day7/amplifiers.js
+++ day7/amplifiers.js
@@ -5,32 +5,94 @@ var input = [
   ,puzzleInput
 ]
 
+var input2 = [
+`3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5`,
+`3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10`
+  ,puzzleInput
+]
 
-/*var Computer = function () {
-  this.program = []
+var Computer = function () {
+  this.ints = []
   this.pc = 0
-  this.amode = 0
-  this.bmode = 0
-  this.cmode = 0
-  this.decodeOp = function() {
-    var fullop = program[this.pc]
+  this.halted = false
+  this.waitingInput = false
+  this.outputReady = false
+  this.outMode = 0
+  this.execute = function() {
+    var fullop = this.ints[this.pc]
     var op = fullop % 100
-    this.amode = Math.floor((fullop / 100) % 10);
-    this.bmode = Math.floor((fullop / 1000) % 10);
-    this.cmode = Math.floor((fullop / 10000) % 10);
-    if (op === 1) {
-      this.sum()
+    var amode = Math.floor((fullop / 100) % 10);
+    var bmode = Math.floor((fullop / 1000) % 10);
+    var cmode = Math.floor((fullop / 10000) % 10);
+
+    if (op === 1) { // sum
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      var c = this.ints[this.pc+3]
+      this.ints[c] = (amode === 0 ? this.ints[a] : a) + (bmode === 0 ? this.ints[b] : b)
+      this.pc += 4
     } else if (op === 2) { // multiply
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      var c = this.ints[this.pc+3]
+      this.ints[c] = (amode === 0 ? this.ints[a] : a) * (bmode === 0 ? this.ints[b] : b)
+      this.pc += 4
+    } else if (op === 3) { // input
+      this.waitingInput = true
+      // call processInput from outside
+    } else if (op === 4) { // output
+      this.outputReady = true
+      this.outMode = amode
+    } else if (op === 5) { // jump-if-true
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      if ((amode === 0 ? this.ints[a] : a) !== 0) {
+        this.pc = (bmode === 0 ? this.ints[b] : b)
+      } else {
+        this.pc += 3
+      }
+    } else if (op === 6) { // jump-if-false
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      if ((amode === 0 ? this.ints[a] : a) === 0) {
+        this.pc = (bmode === 0 ? this.ints[b] : b)
+      } else {
+        this.pc += 3
+      }
+    } else if (op === 7) { // less-than
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      var c = this.ints[this.pc+3]
+      this.ints[c] = (amode === 0 ? this.ints[a] : a) < (bmode === 0 ? this.ints[b] : b) ? 1 : 0
+      this.pc += 4
+    } else if (op === 8) { // equals
+      var a = this.ints[this.pc+1]
+      var b = this.ints[this.pc+2]
+      var c = this.ints[this.pc+3]
+      this.ints[c] = (amode === 0 ? this.ints[a] : a) === (bmode === 0 ? this.ints[b] : b) ? 1 : 0
+      this.pc += 4
+    } else if (op === 99) { // HALT
+      this.pc = -999
+      halted = true
+    } else {
+      console.log('exception exception exception')
+      halted = true
     }
   }
-  this.sum = function() {
-    var a = this.program[pc+1]
-    var b = this.program[pc+2]
-    var c = this.program[pc+3]
-    this.program[c] = (this.amode === 0 ? this.program[a] : a) + (this.bmode === 0 ? this.program[b] : b)
-    this.pc += 4
+  this.processInput = function(val) {
+    var a = this.ints[this.pc+1]
+    this.ints[a] = val
+    this.pc += 2
+    this.waitingInput = false
   }
-}*/
+  this.readOutput = function() {
+    var a = this.ints[this.pc+1]
+    var outVal = (this.outMode === 0 ? this.ints[a] : a)
+    this.pc += 2
+    this.outputReady = false
+    return outVal
+  }
+}
 
 var part1 = function() {
 
@@ -69,14 +131,91 @@ var part1 = function() {
     while (permutations.length > 0) {
 
       var phases = $.map(permutations.shift().split(','), (val => {return Number(val)})) //[1,0,4,3,2]
-      var signals = [0]
+      var outSignals = [0]
       var intInputs = []
 
       while (phases.length > 0) {
-        ints = $.map(numbers, (val => {return Number(val)})) // re read the program
+        var com = new Computer()
+        com.ints = $.map(numbers, (val => {return Number(val)})) // re read the program
         // prepare both inputs
         intInputs.push(phases.shift())
+        intInputs.push(outSignals.shift())
+
+        while (!com.halted && !com.outputReady) {
+          com.execute()
+          if (com.waitingInput) {
+            com.processInput(intInputs.shift())
+          }
+        }
+        if (com.outputReady) {
+          outSignals.push(com.readOutput())
+        }
+      } // phases
+
+      var amplifiedSignal = outSignals.shift()
+      if (maxSignal < amplifiedSignal) {
+        maxSignal = amplifiedSignal
+      }
+    } // permutations
+
+    $('#part1').append(input[i])
+      .append('<br>&emsp;')
+      .append(maxSignal)
+      .append('<br>')
+  }
+}
+
+var part2 = function () {
+
+
+  for (var i = 0; i < input2.length; i++) {
+    var numbers = input2[i].split(/\,+/)
+    var ints
+
+    var permutations = []
+
+    //Generating permutation using Heap Algorithm
+    var heapPermutation = function(array, size) {
+      if (size == 1) { // finish
+        permutations.push(array.join(','))
+      }
+
+      for (var i=0; i<size; i++) {
+        heapPermutation(array, size-1);
+        // if size is odd, swap first and last element
+        if (size % 2 == 1) {
+          var temp = array[0];
+          array[0] = array[size-1];
+          array[size-1] = temp;
+        } else { // If size is even, swap ith and last element
+          var temp = array[i];
+          array[i] = array[size-1];
+          array[size-1] = temp;
+        }
+      }
+    }
+
+    var amplifiers = [5,6,7,8,9]
+    heapPermutation(amplifiers, amplifiers.length)
+
+    var maxSignal = -1
+
+    while (permutations.length > 0) {
+
+      var phases = $.map(permutations.shift().split(','), (val => {return Number(val)})) //[1,0,4,3,2]
+      var signals = [0]
+      var intInputs = []
+
+      while (signals.length > 0) {
+        ints = $.map(numbers, (val => {return Number(val)})) // re read the program
+        // prepare both inputs
+        var nextPhase = phases.shift()
+        intInputs.push(nextPhase)
+        phases.push(nextPhase) // reinsert
         intInputs.push(signals.shift())
+
+        //TODO: Refactor and extract to class
+        // we need to maintain the state (pc) between executions
         var pc = 0
         while (pc >= 0) {
           var fullop = ints[pc]
@@ -150,34 +289,9 @@ var part1 = function() {
       }
     }
 
-    $('#part1').append(input[i])
+    $('#part2').append(input2[i])
       .append('<br>&emsp;')
       .append(maxSignal)
-      .append('<br>')
-  }
-}
-
-
-var part2 = function () {
-
-
-  for (var i = 0; i < input.length; i++) {
-    var orbitsInput = input[i].split(/\s+/)
-    var orbitList = {}
-    $.each(orbitsInput, (idx, val) => {
-      var pair = val.split(')')
-      if (orbitList[pair[0]] === undefined) {
-        orbitList[pair[0]] = []
-      }
-      orbitList[pair[0]].push(pair[1])
-    })
-    // console.log(orbitList)
-
-    var shortestPath = 999999
-
-    $('#part2').append(input[i])
-      .append('<br>&emsp;')
-      .append(shortestPath)
       .append('<br>')
   }
 }
@@ -188,6 +302,6 @@ $(function (){
   $('#main').append('<div id="part1"><h2>part 1</h2></div>')
   part1()
   $('#main').append('<br><div id="part2"><h2>part 2</h2></div>')
-  part2()
+  // part2()
   $('#main').append('<br>')
 })
