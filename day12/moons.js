9b406f9ac1a279e675b8384fe0841a412e4897de
Lucas Rigo <lucas.rigo@sap.com>
2020-01-31 16:40:50 -0300

    implemented day 12 part 2

--- day12/moons.js
+++ day12/moons.js
@@ -94,13 +94,154 @@ function escapeHTML(str) {
 var part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
+    var moonStr = input[i].split(/\n+/)
 
-    $('#part2').append(input[i])
+    var moons = []
+    $.each(moonStr, (idx, val) => {
+      var inArr = val.split(/\<|\>|\,\s|\=/)
+      // 0    1    2     3    4    5    6
+      //["", "x", "-1", "y", "0", "z", "2", ""]
+      moons.push({
+        x: Number(inArr[2]),
+        y: Number(inArr[4]),
+        z: Number(inArr[6]),
+        vel: {
+          x:0,
+          y:0,
+          z:0
+        }
+      })
+    })
+
+    $.each(moons, (idx, m) => {
+      m.initx = m.x
+      m.inity = m.y
+      m.initz = m.z
+    })
+
+    var initPos = moonsPos(moons)
+
+    var limit = 300000 // 0=2772 1=4686774924 2=467081194429464
+
+    var timex = 0
+    while (timex++ < limit) {
+      // apply gravity
+      $.each(moons, (idxa, ma) => {
+        $.each(moons, (idxb, mb) => {
+          if (ma.x < mb.x) {
+            ma.vel.x += 1
+          } else if (ma.x > mb.x) {
+            ma.vel.x -= 1
+          } // if it's the same then it doesn't change
+        })
+      })
+      // apply velocity
+      $.each(moons, (idx, m) => {
+        m.x += m.vel.x
+      })
+      if (moonsEqInit(moons, 'x')) {
+        timex++
+        break;
+      }
+    }
+
+    var timey = 0
+    while (timey++ < limit) {
+      // apply gravity
+      $.each(moons, (idxa, ma) => {
+        $.each(moons, (idxb, mb) => {
+          if (ma.y < mb.y) {
+            ma.vel.y += 1
+          } else if (ma.y > mb.y) {
+            ma.vel.y -= 1
+          } // if it's the same then it doesn't change
+        })
+      })
+      // apply velocity
+      $.each(moons, (idx, m) => {
+        m.y += m.vel.y
+      })
+      if (moonsEqInit(moons, 'y')) {
+        timey++
+        break;
+      }
+    }
+
+    var timez = 0
+    while (timez++ < limit) {
+      // apply gravity
+      $.each(moons, (idxa, ma) => {
+        $.each(moons, (idxb, mb) => {
+          if (ma.z < mb.z) {
+            ma.vel.z += 1
+          } else if (ma.z > mb.z) {
+            ma.vel.z -= 1
+          } // if it's the same then it doesn't change
+        })
+      })
+      // apply velocity
+      $.each(moons, (idx, m) => {
+        m.z += m.vel.z
+      })
+      if (moonsEqInit(moons, 'z')) {
+        timez++
+        break;
+      }
+    }
+    if (timex > limit || timey > limit || timez > limit) {
+      console.log('limit reached!')
+    }
+    console.log(timex, timey, timez)
+
+    var result = lcm(timex,timey,timez)
+
+    // console.log(moons)
+    $('#part2').append(escapeHTML(input[i]))
       .append('<br>&emsp;')
-      .append()
+      .append(result)
       .append('<br>')
   }
 }
+
+var moonsPos = function(moons) {
+  return moons.reduce((acc, val) => {
+    return acc + (val.x + ',' + val.y + ',' + val.z) + '|'
+  },'|')
+}
+
+var moonsEqInit = function(moons, axis) {
+  if (axis === 'x') {
+    return moons.reduce((acc, val) => {
+      return acc && (val.x === val.initx)
+    })
+  } else if (axis === 'y') {
+    return moons.reduce((acc, val) => {
+      return acc && (val.y === val.inity)
+    })
+  } else if (axis === 'z') {
+    return moons.reduce((acc, val) => {
+      return acc && (val.z === val.initz)
+    })
+  } else {
+    return moons.reduce((acc, val) => {
+      return acc && (val.x === val.initx) && (val.y === val.inity) && (val.z === val.initz)
+    })
+  }
+}
+
+// thanks to: https://github.com/romellem/advent-of-code/blob/master/2019/12/moons.js
+
+const gcd = (a, b) => {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
+
+const _lcm = (a, b) => {
+  if (b === 0) return 0;
+  return (a * b) / gcd(a, b);
+};
+
+const lcm = (...args) => args.reduce((a, b) => _lcm(a, b));
 
 $(function (){
   $('#main').append('<div id="part1"><h2>part 1</h2></div>')
